44a4782f40d79f92f94717fae52eba2bafdd382a
JeanMieses <araujo0275@gmail.com>
2021-10-27 12:40:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs')
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/home', (req, res) => {
+    res.render('index');
+})
+
+app.get('/projects', (req, res) => {
+    res.send('PROJECTS!');
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Starting server');
+})
